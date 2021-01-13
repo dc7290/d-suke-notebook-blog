2430b9d3965dd5558efed114892cae0f9002b52d
dc7290 <dhkh.cba0927@icloud.com>
2021-01-13 22:12:38 +0900

    pathpida導入

---
+++ componets/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type Props = {
+
+}
+
+const Component: React.FC<Props> = () => (
+
+)
+
+type ContainerProps = {
+
+}
+
+const Container: React.FC<ContainerProps> = () => {
+
+  return <Component />
+}
+
+export default Container
