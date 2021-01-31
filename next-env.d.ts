025795154a9f96d95f79d0f0f9d32eb9e664dec2
dc7290 <dhkh.cba0927@icloud.com>
2021-01-31 23:37:57 +0900

    google analytics 設定 ユーザー数計測 ページごとに計測 検索されたワード計測

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,22 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+interface Window {
+  // pageviewのため
+  gtag(
+    type: 'config',
+    googleAnalyticsId: string,
+    options: {
+      page_path: string
+    }
+  )
+  // eventのため
+  gtag(
+    type: 'event',
+    eventAction: string,
+    fieldObject: {
+      event_label: string
+      event_category: string
+      value?: string
+    }
+  )
+}
